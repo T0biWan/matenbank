36693655c5c1ac0c12ed88c4b92f4d0375573185
tobi <tobi.klatt@web.de>
2017-12-06 20:34:38 +0100

    added first component

---
+++ src/components/hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="hero is-info">
+    <div class="hero-body">
+      <div class="container">
+        <h2 class="title is-2">{{ title }}</h2>
+        <h5 class="subtitle is-5">{{ subtitle }} </h5>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "hero",
+    props: {
+      title: this.title,
+      subtitle: this.subtitle
+    },
+  }
+</script>
