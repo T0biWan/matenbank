7af244bd8c8dfe9d276dbf0fc3787125f99546f3
tobi <tobi.klatt@web.de>
2017-12-06 20:43:18 +0100

    added hero to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-
+    <hero title="Be my mate" subtitle="Choose your source of energy"></hero>
   </div>
 </template>
 
 <script>
+import json from "./data/data.json"
+import hero from "./components/hero.vue"
+
 export default {
   name: 'app',
+  components: {
+    'hero': hero,
+  },
+  data() {
+    return {
+      data: json
+    };
+  },
 }
 </script>
 
