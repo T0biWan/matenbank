87219f96a6b92a45eca193a6499a139af7d3b1bb
tobi <tobi.klatt@web.de>
2017-12-16 14:46:03 +0100

    try to access api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
-    <hero title="Mate my day" subtitle="Choose your source of energy"></hero>
+    <hero title="Matenbank" subtitle="Choose your source of energy"></hero>
+    <div class="container">
+      <mates></mates>
+    </div>
   </div>
 </template>
 
 <script>
-import json from "./data/data.json"
 import hero from "./components/hero.vue"
+import mates from "./components/mates.vue"
 
 export default {
   name: 'app',
   components: {
     'hero': hero,
+    'mates': mates,
   },
   data() {
     return {
-      data: json
+
     };
   },
 }
---
+++ frontend/src/components/mates.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  {{results}}
+</div>
+</template>
+
+<script>
+const MatenbankBaseURL = "http://127.0.0.1:3000/mate";
+
+export default {
+  name: "mates",
+
+  data() {
+    return {
+      results: []
+    };
+  },
+
+  mounted() {
+    this.getMates();
+  },
+
+  methods: {
+    getMates() {
+      let url = MatenbankBaseURL;
+      axios.get(url).then((response) => {this.results = response.data.results;
+      console.log(response.data.results);})
+      .catch(error => {console.log(error);});
+    },
+  },
+
+};
+</script>
